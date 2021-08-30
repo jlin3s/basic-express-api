a5bfd6300d3356fb3eac9e9d129053baaa03126a
jlin3s <jlines@mediaspec.com.au>
2021-08-30 10:05:46 +1000

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
-app.get('/',(req,res) => res.send('Hello World'));
-app.listen (port, () => console.log('Server is running on port' + port));
+app.get('/',(req,res) => res.send('Hello Worl!!'));
+app.listen (port, () => console.log('Server is running on port ' + port));
